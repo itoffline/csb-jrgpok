11468407ab4bc464ce1ec66a0bc1c76c8e147eed
Code Sandbox <hello@codesandbox.io>
2023-01-06 15:16:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.338ee65ce714b0978567ef5a737060ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2102b4b35d96d9e626dde173d05be41",
+    "url": "https://itoffline.github.io/csb-jrgpok/index.html"
+  },
+  {
+    "revision": "2c61eb965fb1ee71ffad",
+    "url": "https://itoffline.github.io/csb-jrgpok/static/css/main.45fa1d52.chunk.css"
+  },
+  {
+    "revision": "ea7bb9c1fa05866b3134",
+    "url": "https://itoffline.github.io/csb-jrgpok/static/js/2.04cd8e1a.chunk.js"
+  },
+  {
+    "revision": "2c61eb965fb1ee71ffad",
+    "url": "https://itoffline.github.io/csb-jrgpok/static/js/main.17986154.chunk.js"
+  },
+  {
+    "revision": "29e449c8443d78ab4b72",
+    "url": "https://itoffline.github.io/csb-jrgpok/static/js/runtime~main.b33bd1cc.js"
+  }
+]);
